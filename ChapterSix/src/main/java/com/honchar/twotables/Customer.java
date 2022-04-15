d145a8bcdef6e315fedf897cb97f7fb02d72ef8d
Dmytro Honchar <dmytro.honchar972@gmail.com>
2022-04-15 23:59:04 +0300

    Updated module (ChapterFour).

--- ChapterSix/src/main/java/com/honchar/twotables/Customer.java
+++ ChapterSix/src/main/java/com/honchar/twotables/Customer.java
@@ -7,13 +7,15 @@ import jakarta.persistence.*;
         name = "customer",
         uniqueConstraints = {@UniqueConstraint(columnNames = "name")}
 )
-@SecondaryTable(name = "customer_details")
+@SecondaryTable(
+        name = "customer_details"
+)
 public class Customer {
     @Id
     public int id;
     public String name;
     @Column(table = "customer_details")
     public String address;
-    public Customer() {
-    }
+
+    public Customer() {}
 }
